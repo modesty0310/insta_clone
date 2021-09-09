f18f5969605f7666620d40ccb1137927a55dd8df
modesty0310 <rlatprua4@gmail.com>
2021-09-09 22:03:26 +0900

    회원 가입 기능 추가

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
+
+router.post('/join', async (req, res, next) => {
+  const {email, password, chk_password, name, birth, gender, phoneNumber} = req.body;
+  console.log(req.body);
+  try {
+    const user = await User.findOne({email});
+    if(user){
+      return res.status(400).json({message: "이미 존재하는 아이디 입니다."});
+    }
+    if(password !== chk_password) {
+      return res.status(500).json({message: "비밀번호가 일치하지 않습니다."})
+    }
+    const hash = await bcrypt.hash(password, 12);
+    await User.create({
+      email, 
+      password: hash, 
+      name, 
+      birth, 
+      gender, 
+      phoneNumber
+    });
+    res.status(201).json({message: "회원가입이 완료 되었습니다."});
+  } catch (err) {
+    res.status(400).json({message: err.message});
+  }
+  
+})
 
 router.post('/login', async (req,res,next) => {
   try {
